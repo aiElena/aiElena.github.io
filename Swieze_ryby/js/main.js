5ccbe0f1056bb39bc8f5390632f62179a7f19110
aiElena <ai.alexandrova@ukr.net>
2018-03-22 15:46:50 +0200

    button top

--- Swieze_ryby/js/main.js
+++ Swieze_ryby/js/main.js
@@ -30,3 +30,13 @@ $(document).ready(function() {
 	
 
 })
+
+var t;
+function up() {
+  var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+  if(top > 0) {
+    window.scrollBy(0,-100);
+    t = setTimeout('up()',40);
+  } else clearTimeout(t);
+  return false;
+}
