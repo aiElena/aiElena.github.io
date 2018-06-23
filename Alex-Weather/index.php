709a7fbd8df0375171fdfbb5f28014dafc8764dc
aiElena <ai.alexandrova@ukr.net>
2018-06-23 11:28:31 +0300

    delit

--- Alex-Weather/index.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-$url = "http://api.openweathermap.org/data/2.5/weather?id=706483&lang=en&units=metric&appid=853437dd97c1de13d607d8ea9a4cbae4";
-$contents = file_get_contents($url);
-
-echo ($contents);
-
-?>
