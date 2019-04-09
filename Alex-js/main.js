822798a66d0f81369a3fb54acdfaba069afbb5df
aiElena <ai.alexandrova@ukr.net>
2019-04-09 22:55:10 +0300

    text

---
+++ Alex-js/main.js
@@ -0,0 +1,11 @@
+var message = 'Hello!';
+var hello = 'Hello world!';
+var name = "Вася";
+var number = 1;
+var name;
+var year;
+var price = 300;
+const myConst = 3.14;
+const myName = 'Elena';
+let myIncome;
+let 50;
