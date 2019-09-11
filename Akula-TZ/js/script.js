f7bac686589ac95d8b3a47e8ae21ca569d726e51
aiElena <ai.alexandrova@ukr.net>
2019-09-11 15:11:36 +0300

    script

--- Akula-TZ/js/script.js
+++ Akula-TZ/js/script.js
@@ -22,16 +22,24 @@ jQuery(document).ready(function() {
 
   });
     $(function() {
-       $(".second_div").hide();
-            $(".btn1").click(function () {
-                $(".second_div").show().toggle(500);
-                $(".first_div").hide().toggle(500)
-            });
-          $(".btn2").click(function () {
-                $(".first_div").show().toggle(500);
-                $(".second_div").hide().toggle(500)
-        });
-
+        /*$(".second_div").hide();
+		$(".btn1").click(function () {
+			$(".second_div").show().toggle(500);
+			$(".first_div").hide().toggle(500)
+		});
+	    $(".btn2").click(function () {
+			$(".first_div").show().toggle(500);
+			$(".second_div").hide().toggle(500)
+        });*/
+		//можно и так, но так нехорошо, если у вас вдруг будет 4 кнопки, код увеличиться в 2 раза, согласитесь, не оч
+		$('.tab-content:not(.active)').hide(); //скрываем неактивный контент
+		$('.tab-btn').click(function(){
+			$this = $(this); //получаем кнопку
+			$container = $this.parent();
+			//$container.find('.tab-content').removeClass('active'); //это если скрывать через добавление класса
+			$container.find('.tab-content').hide().toggle(500); //то же, только так как у вас
+			$container.find($this.attr('href')).show().toggle(500);
+		});
   });
   
 })(jQuery);
