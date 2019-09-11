6121fcab60bec9d060b5eaf3410805a0d9016b0c
aiElena <ai.alexandrova@ukr.net>
2019-09-11 17:57:08 +0300

    script

--- Akula-TZ/js/script.js
+++ Akula-TZ/js/script.js
@@ -21,31 +21,20 @@ jQuery(document).ready(function() {
     });
 
   });
-    $(function() {
-        /*$(".second_div").hide();
-		$(".btn1").click(function () {
-			$(".second_div").show().toggle(500);
-			$(".first_div").hide().toggle(500)
-		});
-	    $(".btn2").click(function () {
-			$(".first_div").show().toggle(500);
-			$(".second_div").hide().toggle(500)
-        });*/
-		//можно и так, но так нехорошо, если у вас вдруг будет 4 кнопки, код увеличиться в 2 раза, согласитесь, не оч
-		$('.tab-content:not(.active)').hide(); //скрываем неактивный контент
-		$('.tab-btn').click(function(e){
-			e.preventDefault();
-			$this = $(this); //получаем кнопку
-			if(!$this.hasClass('active')){
-				$container = $this.parent();
-				$container.find('.tab-content').removeClass('active'); //это если скрывать через добавление класса
-				$container.find('.tab-content').hide().toggle(500); //то же, только так как у вас
-				$container.find($this.attr('href')).show().toggle(500);
-				$container.find('.tab-btn').removeClass('active');
-				$this.addClass('active');
-			}
-		});
-  });
+    
+	
+	$('.tab-content:not(.active)').hide(); //скрываем неактивный контент
+	$('.tab-btn').click(function(e){
+		e.preventDefault();
+		if(!$(this).hasClass('active')){
+			$container = $(this).parent();
+			$container.find('.tab-content').removeClass('active'); 
+			$container.find('.tab-content').slideUp(500); 
+			$container.find($(this).attr('href')).slideDown(500);
+			$container.find('.tab-btn').removeClass('active');
+			$(this).addClass('active');
+		}
+	});
   
 })(jQuery);
 });
