4704e8671b4af448522e4f5b41bd273d89585d74
aiElena <ai.alexandrova@ukr.net>
2019-09-12 11:45:52 +0300

    menu

--- Akula-TZ/js/script.js
+++ Akula-TZ/js/script.js
@@ -15,7 +15,7 @@ jQuery(document).ready(function() {
     $('.menuToggle').on('click', function() {
       	$('.top-menu').slideToggle(300, function(){
 		if($(this).css('display') === 'none'){
-			$(this).closest('.menu').toggleClass('menu_state_open');
+			$(this).closest('.top-menu').toggleClass('menu_state_open');
 		}
 		});
     });
